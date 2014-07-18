53d79f887d897246b1c41c9c86961c9fac168065
thepeg <marco.pegoraro@gmail.com>
2014-07-18 16:17:04 +0200

    codebase

---
+++ Gruntfile.js
@@ -0,0 +1,177 @@
+/**
+ * NoBlackMagic - Building Tool
+ * ============================
+ *
+ *
+ */
+
+module.exports = function (grunt) {
+    
+    
+// --------------------------------------- //
+// ---[[   G R U N T   C O N F I G   ]]--- //
+// --------------------------------------- //
+    
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+        
+        clean: {
+			assets: [
+                'assets/css',
+                'assets/js'
+            ]
+		},
+        
+        less: {
+            build: {
+                options: {
+                    sourceMap: true,
+                    sourceMapFilename: 'build/jquery.jsbox.css.map',
+                    sourceMapRootpath: '../'
+                },
+                files: {
+                    'assets/css/nobm.min.css' : 'src/less/nobm.less'
+                }
+            },
+            release: {
+                files: {
+                    'assets/css/nobm.css' : 'src/less/nobm.less'
+                }
+            },
+            releasemin: {
+                options: {
+                    compress: true,
+                    cleancss: true
+                },
+                files: {
+                    'assets/css/nobm.min.css' : 'src/less/nobm.less'
+                }
+            }
+        },
+        
+        uglify: {
+            build: {
+                options: {
+                    mangle: false,
+                    compress: false,
+                    beautify: true,
+                    preserveComments: true,
+                    sourceMap: true,
+                    sourceMapFilename: 'nobm.js.map'
+                },
+                files: {
+                    'assets/js/mpeg.min.js' : ['src/js/mpeg.js']
+                }
+            },
+            release: {
+                options: {
+                    mangle: false,
+                    compress: false,
+                    beautify: true,
+                    preserveComments: true
+                },
+                files: {
+                    'assets/js/mpeg.js' : ['src/js/mpeg.js']
+                }
+            },
+            releasemin: {
+                files: {
+                    'assets/js/mpeg.min.js' : ['src/js/mpeg.js']
+                }
+            }
+        },
+        
+        usebanner: {
+            build: {
+                options: {
+                    position: 'top',
+                    banner: '/* NoBlackMagic.com v<%= pkg.version %> - DEVELOPEMENT VERSION | by Marco Pegoraro | https://github.com/NoBlackMagic/noblackmagic.github.io */'
+                },
+                files: {
+                    src: [
+                        'assets/css/*.css', 
+                        'assets/js/*.js'
+                    ]
+                }
+            },
+            release: {
+                options: {
+                    position: 'top',
+                    banner: '/* NoBlackMagic.com v<%= pkg.version %> | by Marco Pegoraro | https://github.com/NoBlackMagic/noblackmagic.github.io */'
+                },
+                files: {
+                    src: [
+                        'assets/css/*.css', 
+                        'assets/js/*.js'
+                    ]
+                }
+            }
+        },
+            
+        watch: {
+            build: {
+                files: ['src/**/*'],
+                tasks: ['build']
+            }
+        }
+		
+	});
+    
+    
+    
+    
+    
+// ----------------------------------------------- //
+// ---[[   P U B L I C   I N T E R F A C E   ]]--- //
+// ----------------------------------------------- //
+    
+    grunt.registerTask('build', [
+        'clean:assets',
+        'less:build',
+        'uglify:build',
+        'usebanner:build'
+    ]);
+    
+    grunt.registerTask('release', [
+        'clean:assets',
+        'less:release',
+        'less:releasemin',
+        'uglify:release',
+        'uglify:releasemin',
+        'usebanner:release'
+    ]);
+        
+    grunt.registerTask('develop', [
+        'build',
+        'watch:build'
+    ]);
+    
+    grunt.registerTask('default', ['build']);
+    
+    
+    
+    
+    
+// --------------------------------------------------- //
+// ---[[   L O A D   G R U N T   M O D U L E S   ]]--- //
+// --------------------------------------------------- //
+    
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-banner');
+    
+
+    
+    
+// --------------------------------------- //
+// ---[[   C U S T O M   T A S K S   ]]--- //
+// --------------------------------------- //
+    
+    
+    
+};
---
+++ src/js/mpeg.js
@@ -0,0 +1,23 @@
+
+(function(){
+    
+    var html =  '<div class="author-block">' +
+                    '<h3>Marco Pegoraro <small>Lead Front End Dev</small></h3>' +
+                    '<ul class="clearfix">' +
+                        '<li>' +
+                            '<img src="//assets/images/marco-pegoraro.jpg" alt="Marco Pegoraro" width="80" class="frame left" />' +
+                            '<p style="padding-top:20px">' +
+                                'I play with Javascript, HTML5, mobile web apps, NodeJS, airplanes.' +
+                            '</p>' +
+                            '<p style="clear:both;margin-top: 15px;">' +
+                                '<a href="http://twitter.com/thepeg" target="_blank">@ThePeg</a>' +
+                                ' | ' +
+                                '<a href="http://noblackmagic.com" target="_blank">NoBlackMagic.com</a>' +
+                            '</p>' +
+                        '</li>' +
+                    '</ul>' +
+                '</div>';
+    
+    document.write(html);
+    
+})();
