eb82d9e68905a0165817929b67a2d07cbe809ef9
thepeg <marco.pegoraro@gmail.com>
2014-08-03 15:15:17 +0200

    fixed author box style

--- src/js/mpeg.js
+++ src/js/mpeg.js
@@ -1,22 +1,20 @@
 
 (function(){
     
-    var html =  '<div class="author-block">' +
+    var html =  '<aside class="author-block">' +
                     '<h3>Marco Pegoraro <small>Lead Front End Dev</small></h3>' +
-                    '<ul class="clearfix">' +
-                        '<li>' +
-                            '<img src="http://noblackmagic.com/assets/images/marco-pegoraro.jpg" alt="Marco Pegoraro" width="80" class="frame left" />' +
-                            '<p style="padding-top:20px">' +
-                                'I play with Javascript, HTML5, mobile web apps, NodeJS, airplanes.' +
-                            '</p>' +
-                            '<p style="clear:both;margin-top: 15px;">' +
-                                '<a href="http://twitter.com/thepeg" target="_blank">@ThePeg</a>' +
-                                ' | ' +
-                                '<a href="http://noblackmagic.com" target="_blank">NoBlackMagic.com</a>' +
-                            '</p>' +
-                        '</li>' +
-                    '</ul>' +
-                '</div>';
+                    '<div>' +
+                        '<img src="//noblackmagic.com/assets/images/marco-pegoraro.jpg" alt="Marco Pegoraro" width="80" class="frame left" />' +
+                        '<p style="padding-top:20px">' +
+                            'I play with Javascript, HTML5, mobile web apps, NodeJS, airplanes.' +
+                        '</p>' +
+                        '<p class="clearfix" style="margin-top: 15px;">' +
+                            '<a href="http://twitter.com/thepeg" target="_blank">@ThePeg</a>' +
+                            ' | ' +
+                            '<a href="http://noblackmagic.com" target="_blank">NoBlackMagic.com</a>' +
+                        '</p>' +
+                    '</div>' +
+                '</aside>';
     
     document.write(html);
     
